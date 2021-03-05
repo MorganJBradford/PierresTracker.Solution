c48bd5c64bd29a2dbe4331937635e3faf9dab5c2
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-05 12:20:11 -0600

    SetDescription; pass

--- PierresTracker/Models/Vendor.cs
+++ PierresTracker/Models/Vendor.cs
@@ -3,7 +3,7 @@ namespace PierresTracker.Models
   public class Vendor
   {
     public string Name { get; set; }
-    public string Description { get; }
+    public string Description { get; set; }
 
     public Vendor(string vendorName, string description)
     {
