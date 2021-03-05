62f5f02854ae04b0f5d04689473ead33a4bb707e
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-05 10:59:51 -0600

    SetPrice; pass

--- PierresTracker/Models/Order.cs
+++ PierresTracker/Models/Order.cs
@@ -4,12 +4,13 @@ namespace PierresTracker
   {
     public string Title { get; set; }
     public string Description { get; set; }
-    public int Price { get; }
+    public int Price { get; set; }
 
     public Order (string title, string description, int price)
     {
       Title = title;
       Description = description;
+      Price = price;
     }
   }
 }
