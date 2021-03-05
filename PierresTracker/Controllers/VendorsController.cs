6559f6b6dbe111e9940d403b4ce876227b3ff556
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-05 13:18:34 -0600

    add New/Create methods; add new view

--- PierresTracker/Controllers/VendorsController.cs
+++ PierresTracker/Controllers/VendorsController.cs
@@ -12,5 +12,18 @@ namespace PierresTracker.Controllers
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
   }
 }
